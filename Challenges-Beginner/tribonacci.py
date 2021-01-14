78f26a1d3f7e972b9323a55c14a96d9cc1972f93
Tobias Kiehnlein <tau.kappa.2000@gmail.com>
2021-01-14 15:38:14 +0000

    Add fix for length<3 in tribonacci

--- Challenges-Beginner/tribonacci.py
+++ Challenges-Beginner/tribonacci.py
@@ -1,8 +1,8 @@
 #works like fibonacci only with a base length of 3
 def tribonacci(signature, n):
-    while len(signature) != n:
+    while len(signature) < n:
         signature.append(signature[-1] + signature[-2] + signature[-3])
-    return signature
+    return signature[:n]
 
 
-print(tribonacci([0, 0, 1], 20))
+print(tribonacci([0, 0, 1], 2))
