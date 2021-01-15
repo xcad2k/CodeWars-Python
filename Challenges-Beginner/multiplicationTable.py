fca79cd7fb2c5f6c7a7ab6bd9879562e334966d4
Christian <christian@the-digital-life.com>
2021-01-15 13:53:48 +0000

    c

--- Challenges-Beginner/multiplicationTable.py
+++ Challenges-Beginner/multiplicationTable.py
@@ -1,2 +1,4 @@
 def multiplication_table(size):
-    return [[x + y - 1 if x == 1 or y == 1 else x * y for y in range(1, size + 1)] for x in range(1, size + 1)]
+    return [[x * y for y in range(1, size + 1)] for x in range(1, size + 1)]
+
+print(multiplication_table(3))
---
+++ Challenges-Beginner/order.py
@@ -0,0 +1,27 @@
+import re
+
+
+def order(sentence):
+    if sentence == "": return ""
+    result = sentence.split(' ')
+    for word in sentence.split(' '):
+        result[int(re.findall('\d+', word)[0]) - 1] = word
+    return " ".join(result)
+
+
+#same solution without regex
+def order2(sentence):
+    if sentence == "": return ""
+    result = sentence.split(' ')
+    for word in sentence.split(' '):
+        result[int("".join([c for c in word if c.isdigit()])) - 1] = word
+    return " ".join(result)
+
+#One line solution... its based on the fact that numbers will be sorted before other chars
+def order3(sentence):
+  return ' '.join(sorted(sentence.split(), key=lambda w:sorted(w)))
+
+
+print(order("4of Fo1r pe6ople g3ood th5e the2"))
+print(order2("4of Fo1r pe6ople g3ood th5e the2"))
+print(order3("4of Fo1r pe6ople g3ood th5e the2"))
