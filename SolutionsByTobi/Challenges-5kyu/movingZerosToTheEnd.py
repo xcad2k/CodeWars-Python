7c341bd030dd244d5996721432ee161c9ac2f47a
Tobias Kiehnlein <tobias@kiehnlein.com>
2021-01-27 17:08:14 +0100

    Add second solution to "Moving zeros to the end)

--- SolutionsByTobi/Challenges-5kyu/movingZerosToTheEnd.py
+++ SolutionsByTobi/Challenges-5kyu/movingZerosToTheEnd.py
@@ -1,2 +1,5 @@
 def move_zeros(array):
     return sorted(array, key=lambda x: 1 if x == 0 and not x is False else 0)
+
+def move_zeros2(array):
+    return [x for x in array if x != 0 or x is False] + [x for x in array if x == 0 and x is not False]
